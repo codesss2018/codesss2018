a709397299e19d8c280b372f71062b01dab1bc1f
codesss2018 <codesss2018@gmail.com>
2018-05-16 02:57:44 +0000

    changes

--- resources/views/commons/slider.blade.php
+++ resources/views/commons/slider.blade.php
@@ -1,21 +1,21 @@
 <div id="da-slider" class="da-slider text-center">
     <div class="da-slide">
-        <h2>education portal</h2>
+        <h2>anynone anywhere</h2>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<span class="hide_text"> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span></p>
         <h3 class="da-link"><a href="" class="fa-btn btn-1 btn-1e">view more</a></h3>
     </div>
     <div class="da-slide">
-        <h2>online educations</h2>
+        <h2>come code</h2>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<span class="hide_text">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span></p>
         <h3 class="da-link"><a href="" class="fa-btn btn-1 btn-1e">view more</a></h3>
     </div>
     <div class="da-slide">
-        <h2>education portal</h2>
+        <h2>your dreams</h2>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<span class="hide_text"> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span></p>
         <h3 class="da-link"><a href="" class="fa-btn btn-1 btn-1e">view more</a></h3>
     </div>
     <div class="da-slide">
-        <h2>online educations</h2>
+        <h2>with code-s</h2>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<span class="hide_text"> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span></p>
         <h3 class="da-link"><a href="" class="fa-btn btn-1 btn-1e">view more</a></h3>
     </div>
