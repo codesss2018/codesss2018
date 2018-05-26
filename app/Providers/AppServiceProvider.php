97de7b2dde3f0c862369d8f44295dd1f697d838d
codesss2018 <codesss2018@gmail.com>
2018-05-26 21:31:50 +0000

    hero

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Routing\UrlGenerator;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,12 +12,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         //
-        // if(env('APP_ENV') !== 'local'){
-        //     $url->forceSchema('https');
-        // }
+        if(env('APP_ENV') !== 'local'){
+            $url->forceScheme('https');
+        }
     }
 
     /**
